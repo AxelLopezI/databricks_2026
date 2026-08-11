266a7e8622fcdc035546f0ad43e46d434934a115
axl.dxn@gmail.com <axl.dxn@gmail.com>
2026-08-11 23:06:26 +0000

    feat: bronze layer implementation

---
+++ lakehouse_2026/code/bronze/bronze_config.py
@@ -0,0 +1,38 @@
+INGESTION_CONFIG = [
+    {
+        "folder": "source_crm",
+        "name": "cust_info",
+        "extension": "csv",
+        "table": "crm_cust_info"
+    },
+    {
+        "folder": "source_crm",
+        "name": "prd_info",
+        "extension": "csv",
+        "table": "crm_prd_info"
+    },
+    {
+        "folder": "source_crm",
+        "name": "sales_details",
+        "extension": "csv",
+        "table": "crm_sales_details"
+    },
+    {
+        "folder": "source_erp",
+        "name": "CUST_AZ12",
+        "extension": "csv",
+        "table": "erp_cust_az"
+    },
+    {
+        "folder": "source_erp",
+        "name": "LOC_A101",
+        "extension": "csv",
+        "table": "erp_loc_a"
+    },
+    {
+        "folder": "source_erp",
+        "name": "PX_CAT_G1V2",
+        "extension": "csv",
+        "table": "erp_px_cat"
+    }
+]
